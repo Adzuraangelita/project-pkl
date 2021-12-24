acc51bbc8f5bcf036f6b588ee0722510125b604b
Adzuraangelita <adzuraangel13@gmail.com>
2021-12-24 16:46:36 +0700

    role akses

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,19 @@
-@extends('layouts.app')
+@extends('adminlte::page')
 
+@section('title', 'Dashboard')
+
+@section('content_header')
+
+Dashboard
+
+@stop
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+Welcom to this beautiful admin panel
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@stop
+@section('css')
 
-                    @role('admin')
-                    Ini untuk Admin
-                    @endrole
 
-                    @role('pengguna')
-                    Ini untuk pengguna
-                    @endrole
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@section('js')
+@stop
 @endsection
