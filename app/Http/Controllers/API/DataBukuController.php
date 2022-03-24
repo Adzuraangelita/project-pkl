48c91a5f95e9671151d90cb08af0a5ccdf8b35b4
Adzuraangelita <adzuraangel13@gmail.com>
2022-03-24 12:36:11 +0700

    api

--- app/Http/Controllers/API/DataBukuController.php
+++ app/Http/Controllers/API/DataBukuController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\Data_peserta;
+use App\Models\buku;
 use Illuminate\Http\Request;
 
 class DataBukuController extends Controller
